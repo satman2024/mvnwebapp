f5376911dffd546153f57d0519bf1d3f2e008b47
Chuks <saghulor@gmail.com>
2024-08-26 12:34:51 +0100

    commit - updated all my project files

--- myclass.java
+++ myclass.java
@@ -1,5 +1,14 @@
+/**
+ * MyClass is a simple Java program that prints "Hello, World!" to the console.
+ */
 public class MyClass {
+
+    /**
+     * The main method is the entry point of the program.
+     * @param args Command-line arguments (not used in this program).
+     */
     public static void main(String[] args) {
+        // Print a message to the console
         System.out.println("Hello, World!");
     }
 }
